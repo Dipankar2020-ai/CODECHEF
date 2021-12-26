5c4f6b49b8948f36168525686c50814b10ee8321
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-26 21:41:03 +0530

    Create Add Natural Numbers.cpp

---
+++ Dsa Learning Series/Add Natural Numbers.cpp
@@ -0,0 +1,13 @@
+Link:
+https://www.codechef.com/CCSTART2/problems/ADDNATRL
+Solution:
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n;
+	cin>>n;
+	long long ans=(n*(n+1))/2;
+	cout<<ans<<endl;
+	return 0;
+}
