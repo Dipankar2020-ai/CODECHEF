f75bcd67f9a1598563d0dcdc81708af3a2eb3db3
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-27 09:29:34 +0530

    Create Triangle With Angle.cpp

---
+++ Dsa Learning Series/Triangle With Angle.cpp
@@ -0,0 +1,19 @@
+Link->
+https://www.codechef.com/CCSTART2/problems/ANGTRICH
+Solution:->
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a+b+c==180 && a>0 && b>0 && c>0)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	return 0;
+}
