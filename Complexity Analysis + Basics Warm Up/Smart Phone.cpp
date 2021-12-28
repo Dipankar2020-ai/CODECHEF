0b6042558db150aa4908ce95e763a9a63de3169b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-28 11:30:41 +0530

    Create Smart Phone.cpp

---
+++ Complexity Analysis + Basics Warm Up/Smart Phone.cpp
@@ -0,0 +1,28 @@
+Link->
+https://www.codechef.com/LRNDSA01/problems/ZCO14003
+Solution->
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long int n;
+	cin>>n;
+	long long int arr[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	long long int x=0;
+	for(int i=0;i<n;i++)
+	{
+	   long long int temp=arr[i]*(n-i);
+	    if(temp>x)
+	    {
+	        x=temp;
+	    }
+	}
+	cout<<x<<endl;
+	
+	return 0;
+}
